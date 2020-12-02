960cf9246d6a698278a32f0f8be5ade3f5248f78
madiwa Simon <mad.simon01@gmail.com>
2020-12-02 18:55:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word (string)
   reversed = string.split(" ")
   new_reversed = []
   reversed.each do |words|
-  new_reversed << words.reverse
+  new_reversed << words.reverse + " "
   end
-  new_reversed
+  new_reversed.join 
 end
